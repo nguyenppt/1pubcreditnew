0d53bca5c8b2e3b95136074154e8f9994623b8aa
usr.nghiale <usr.nghiale@gmail.com>
2014-12-12 16:27:56 +0000

    Add referenceid to table and update get and update store

---
+++ dbscripts/Alter_Load_Contingent_Account.sql
@@ -0,0 +1,36 @@
+USE [bisolutions_vvcb]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Load_Contingent_Account]    Script Date: 12/12/2014 11:17:14 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[Load_Contingent_Account](@ContigentAcct nvarchar(20))
+AS
+SELECT [CollateralInfoID]
+      ,[ContingentEntryID],cui.[GBStreet]+', '+cui.[GBDist]+', '+cui.[TenTinhThanh]+', '+cui.[CountryName] as Address_cont
+	  ,cui.[DocID] , convert(date, cui.DocIssueDate,103) as DocIssueDate
+      ,coe.[CustomerID]
+      ,[CustomerAddress]
+      ,[DocIDTaxCode]
+      ,[DateOfIssue]
+      ,[TransactionCode]
+      ,[TransactionName]
+      ,coe.[Currency]
+      ,[AccountNo]
+      ,[AccountName]
+      ,[Amount]
+      ,[DealRate]
+      ,[ValueDate] as ValueDateCont
+      ,[Narrative]
+      ,[DCTypeCode]
+      ,[DCTypeName]
+      ,[CollateralType_Code]
+	  , [ReferenceID] from [dbo].[BCOLLATERALCONTINGENT_ENTRY] as coe JOIN [dbo].[BCUSTOMER_INFO] as cui on coe.[CustomerID] = cui.CustomerID
+	   where @ContigentAcct= [CollateralInfoID]
+GO
+
+
